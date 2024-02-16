7bf4f706c5d83fc7d51501b2ff38d4fc9d0015ba
Danial-n <danial.aminorazmi@gmail.com>
2024-02-16 11:12:43 +0800

    added edit title

--- app/components/task/EditList.tsx
+++ app/components/task/EditList.tsx
@@ -10,49 +10,73 @@ import taskStore from './taskStore';
 import { observer } from 'mobx-react';
 
 const EditList = observer(() => {
-  const list = taskStore.list;
+  const storeList = taskStore.list;
   const selectedIndex = taskStore.selectedIndex;
+  const [title, setTitle] = useState<string>(storeList[selectedIndex].title);
+  const [task, setTask] = useState<string[]>(storeList[selectedIndex].tasks);
 
-  const editList = (index: number, title: string) => {
-    list[index].title = title;
-    // const updatedTasks = [...taskStore.userTaskInput];
-    // updatedTasks[index] = task;
-    taskStore.setUserTitleInput(list[index].title);
-    taskStore.setUserTaskInput([...list[index].tasks]);
+  const editTitle = (newTitle: string) => {
+    setTitle(newTitle);
+    console.log(title);
   };
 
-  // const addTask = () => {
-  //   taskStore.setUserTaskInput([...taskStore.userTaskInput, '']);
-  // };
+  const editTask = (newTask: string, index: number) => {
+    const updatedTasks = [...task];
+    updatedTasks[index] = newTask;
+    setTask(updatedTasks);
+    console.log(task);
+  };
 
-  // const updateTask = (index: number, value: string) => {
-  //   const updatedTasks = [...userTaskInput];
-  //   updatedTasks[index] = value;
-  //   setUserTaskInput(updatedTasks);
-  // }
+  const editList = () => {
+    storeList[selectedIndex].title = title;
+    storeList[selectedIndex].tasks = task;
+    taskStore.setList(storeList);
+  };
 
-  // const addTask = () => {
-  //   if (editingIndex !== null) {
-  //     // If editing, update the existing task
-  //     const updatedList = [...storeList];
-  //     updatedList[editingIndex] = {
-  //       title: userTitleInput,
-  //       tasks: userTaskInput,
-  //     };
-  //     setList(updatedList);
+  const formTitle = () => {
+    return (
+      <div className='flex  space-x-3 items-center'>
+        <p>Title:</p>
+        <input
+          type='text'
+          value={title}
+          onChange={(e) => editTitle(e.target.value)}
+          placeholder='Add title here'
+        />
+      </div>
+    );
+  };
 
-  //     taskStore.setEditingIndex(null);
-  //   } else {
-  //     // If not editing, add a new task
-  //     setUserTaskInput([...userTaskInput, '']);
-  //   }
-  // };
+  const formDetail = () => {
+    return (
+      <div className='flex flex-col space-y-3'>
+        <div className='flex justify-between items-center'>
+          <p>Task(s):</p>
+        </div>
+        {/* TASK LIST */}
+        {storeList[selectedIndex].tasks.map((taskIndex: any) => (
+          <div key={taskIndex} className='flex justify-between items-center'>
+            {/* TASK INPUT */}
+            <input
+              value={task}
+              onChange={(e) => editTask(e.target.value, taskIndex)}
+              placeholder='Add details here'
+            />
+            {/* DELETE TASK */}
+            <button
+              // onClick={() => deleteItem(index)}
+              className=' size-8 rounded-md bg-neutral-500'
+            >
+              -
+            </button>
+          </div>
+        ))}
 
-  // const updateTask = (index: number, value: string) => {
-  //   const updatedTasks = [...userTaskInput];
-  //   updatedTasks[index] = value;
-  //   setUserTaskInput(updatedTasks);
-  // };
+        {/* ADD TASK BTN */}
+        <button className=' rounded-md bg-neutral-500'>more tasks</button>
+      </div>
+    );
+  };
 
   return (
     <Dialog>
@@ -62,60 +86,12 @@ const EditList = observer(() => {
       <DialogContent>
         <div className='w-full space-y-3'>
           <h2 className='flex items-center justify-center'>Edit</h2>
-          {/* FORM TITLE */}
-          <div className='flex  space-x-3 items-center'>
-            <p>Title:</p>
-            <input
-              key={selectedIndex}
-              type='text'
-              value={list[selectedIndex].title}
-              onChange={(e) => editList(selectedIndex, e.target.value)}
-              placeholder='Add title here'
-            />
-          </div>
-          {/* FORM DETAIL */}
-          <div className='flex flex-col space-y-3'>
-            <div className='flex justify-between items-center'>
-              <p>Task(s):</p>
-            </div>
-
-            {/* TASK LIST */}
-            {list[selectedIndex].tasks.map((item: string, taskIndex: any) => (
-              <div
-                key={taskIndex}
-                className='flex justify-between items-center'
-              >
-                {/* TASK INPUT */}
-                <input
-                  value={item}
-                  // onChange={handleTitle}
-                  placeholder='Add details here'
-                />
-                {/* DELETE TASK */}
-                <button
-                  // onClick={() => deleteItem(index)}
-                  className=' size-8 rounded-md bg-neutral-500'
-                >
-                  -
-                </button>
-              </div>
-            ))}
-
-            {/* ADD TASK BTN */}
-            <button
-              // onClick={addTask}
-              className=' rounded-md bg-neutral-500'
-            >
-              more tasks
-            </button>
-          </div>
+          {formTitle()}
+          {formDetail()}
         </div>
         <div className='w-full flex items-center justify-center'>
           {/* ADD FORM TO LIST BTN */}
-          <DialogClose
-            // onClick={() => editList(index)}
-            className=' w-16 border '
-          >
+          <DialogClose onClick={editList} className='w-16 border'>
             Edit
           </DialogClose>
         </div>
